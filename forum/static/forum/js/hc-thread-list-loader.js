93a1e9adfb60b1558983fee8c633413eceff7c97
Moandor <moandor.y@gmail.com>
2016-08-28 15:44:39 +0800

    Link to thread details

--- forum/static/forum/js/hc-thread-list-loader.js
+++ forum/static/forum/js/hc-thread-list-loader.js
@@ -9,6 +9,7 @@ function loadThreadList(boardId, page) {
         var threadInfo;
         var threadContent;
         var span;
+        var hyperlink;
         var length;
         var list = document.getElementById("hc-thread-list");
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
@@ -18,28 +19,41 @@ function loadThreadList(boardId, page) {
                 item = document.createElement("div");
                 threadInfo = document.createElement("div");
                 threadInfo.className = "hc-thread-info";
+
                 span = document.createElement("span");
                 span.className = "hc-thread-info-title";
                 span.innerHTML = threads[i].title;
                 threadInfo.appendChild(span);
+
                 span = document.createElement("span");
                 span.className = "hc-thread-info-username";
                 span.innerHTML = threads[i].username;
                 threadInfo.appendChild(span);
+
                 span = document.createElement("span");
                 span.className = "hc-thread-info-time";
                 span.innerHTML = threads[i].created_at;
                 threadInfo.appendChild(span);
+
                 span = document.createElement("span");
                 span.className = "hc-thread-info-uid";
                 span.innerHTML = threads[i].user;
                 threadInfo.appendChild(span);
+
+                hyperlink = document.createElement("a");
+                hyperlink.setAttribute("href", "/forum/t/" + threads[i].id + "/");
+                hyperlink.innerHTML = "回应";
+                threadInfo.appendChild(hyperlink);
+
                 item.appendChild(threadInfo);
+
                 threadContent = document.createElement("div");
                 threadContent.className = "hc-thread-content";
                 threadContent.innerHTML = threads[i].text;
                 item.appendChild(threadContent);
+
                 list.appendChild(item);
+                list.appendChild(document.createElement("hr"));
             }
         }
     }
