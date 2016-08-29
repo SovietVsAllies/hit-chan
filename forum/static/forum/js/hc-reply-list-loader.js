9d61504eed0c72d1ea1bb3faa589000886c67b7f
ANDI-Mckee <mckee4working@gmail.com>
2016-08-29 10:44:38 +0800

    bug fixes

--- forum/static/forum/js/hc-reply-list-loader.js
+++ forum/static/forum/js/hc-reply-list-loader.js
@@ -83,7 +83,7 @@ function loadReplyList(threadId, page) {
 	            list.appendChild(item);
                 list.appendChild(document.createElement("hr"));
             }
-            generatePageNavigation(page, threadAndReplies.reply_count);
+            generatePageNavigation(page, ceil(threadAndReplies.reply_count / 20));
 		}
 	}
 	xmlHttp.open("GET", "/forum/api/thread/?id=" + threadId + "&page=" + page, true);
