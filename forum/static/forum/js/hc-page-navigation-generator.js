507f5c43af6dee3cbbb65daa4ea14dd5765279d9
ANDI-Mckee <mckee4working@gmail.com>
2016-08-29 09:05:39 +0800

    a little modification

--- forum/static/forum/js/hc-page-navigation-generator.js
+++ forum/static/forum/js/hc-page-navigation-generator.js
@@ -7,45 +7,57 @@ function generatePageNavigation(currentPage, maxPage) {
     var item;
     var span;
     var hyperlink;
-    if (pageStart != 1) {
+    // Create a button to the first page if necessary.
+    if (pageStart > 1) {
         item = document.createElement("li");
+        item.className = "hc-page-enabled";
+        span = document.createElement("span");
         hyperlink = document.createElement("a");
         hyperlink.setAttribute("href", "../1/");
         hyperlink.innerHTML = "首页";
-        item.appendChild(hyperlink);
+        span.appendChild(hyperlink);
+        item.appendChild(span);
         pageNavigation.appendChild(item);
     }
+    // Create a button to the previous page.
     item = document.createElement("li");
-    if (currentPage <= 1) {
+    if (currentPage <= 1) {     // Disable the button with no previous page.
         item.className = "hc-page-disabled";
         span = document.createElement("span");
         span.innerHTML = "上一页";
         item.appendChild(span);
         pageNavigation.appendChild(item);
     } else {
+        span = document.createElement("span");
         hyperlink = document.createElement("a");
         hyperlink.setAttribute("href", "../" + (currentPage - 1) + "/");
         hyperlink.innerHTML = "上一页";
-        item.appendChild(hyperlink);
+        span.appendChild(hyperlink);
+        item.appendChild(span);
         pageNavigation.appendChild(item);
     }
+    // Create buttons with page number on.
     var i;
     for (i = pageStart; i <= pageEnd; i++) {
         item = document.createElement("li");
         if (i == currentPage) {
-            item.className = "hc-page-active";
+            item.className = "hc-page-onload";
             span = document.createElement("span");
             span.innerHTML = i;
             item.appendChild(span);
             pageNavigation.appendChild(item);
         } else {
+            item.className = "hc-page-enabled";
+            span = document.createElement("span");
             hyperlink = document.createElement("a");
             hyperlink.setAttribute("href", "../" + i + "/");
             hyperlink.innerHTML = i;
-            item.appendChild(hyperlink);
+            span.appendChild(hyperlink);
+            item.appendChild(span);
             pageNavigation.appendChild(item);
         }
     }
+    // Create a button to the next page.
     item = document.createElement("li");
     if (currentPage >= maxPage) {
         item.className = "hc-page-disabled";
@@ -54,18 +66,25 @@ function generatePageNavigation(currentPage, maxPage) {
         item.appendChild(span);
         pageNavigation.appendChild(item);
     } else {
+        item.className = "hc-page-enabled";
+        span = document.createElement("span");
         hyperlink = document.createElement("a");
         hyperlink.setAttribute("href", "../" + (currentPage + 1) + "/");
         hyperlink.innerHTML = "下一页";
-        item.appendChild(hyperlink);
+        span.appendChild(hyperlink);
+        item.appendChild(span);
         pageNavigation.appendChild(item);
     }
+    // Create a button to the last page if necessary.
     if (pageEnd != maxPage) {
         item = document.createElement("li");
+        item.className = "hc-page-enabled";
+        span = document.createElement("span");
         hyperlink = document.createElement("a");
         hyperlink.setAttribute("href", "../" + maxPage + "/");
         hyperlink.innerHTML = "末页";
-        item.appendChild(hyperlink);
+        span.appendChild(hyperlink);
+        item.appendChild(span);
         pageNavigation.appendChild(item);
     }
 }
